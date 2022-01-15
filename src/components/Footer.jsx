be69ae5641c8abb277418189f0588c68c041631c
Sahin <sahinarslantrk@icloud.com>
2022-01-15 15:39:26 +0300

    React E-Commerce App Design - Frontend added.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,136 @@
+import { Facebook, Instagram, Pinterest, Twitter } from '@material-ui/icons';
+import styled from 'styled-components'
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PhoneIcon from '@mui/icons-material/Phone';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import {mobile} from "../responsive";
+
+const Container =styled.div`
+    display: flex;
+    ${mobile({flexDirection:"column"})};
+`;
+const Left =styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`;
+
+const Logo =styled.h1`
+
+`;
+const Desc =styled.p`
+margin:20px 0px;
+`;
+const SocialContainer =styled.div`
+    display: flex;
+`;
+const SocialIcon =styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color:white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+const Center =styled.div`
+    flex: 1;
+    padding: 20px;
+    ${mobile({display:"none"})};
+`;
+const Right =styled.div`
+    flex: 1;
+    padding: 20px;
+    ${mobile({backgroundColor:"#fff8f8"})};
+
+`;
+
+const ContactItem =styled.div`
+margin-bottom: 20px;
+display: flex;
+align-items: center;
+`;
+
+const Payment = styled.img`
+width: 50%;
+`;
+
+
+const Title=styled.h3`
+    margin-bottom: 30px;
+`;
+
+const List=styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+
+`;
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`;
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>SAHIN A.</Logo>
+                <Desc>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis adipisci nostrum earum, sint quasi quo, ullam obcaecati perferendis quaerat temporibus deleniti?
+                    Eius illo ducimus nesciunt maiores ex odit officia doloremque?
+                </Desc>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <Facebook/>
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram/>
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE" >
+                        <Twitter/>
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <Pinterest/>
+                    </SocialIcon>
+                </SocialContainer>
+            </Left>
+            <Center>
+
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Man Fashion</ListItem>
+                    <ListItem>Woman Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem>
+                    <LocationOnIcon style={{marginRight:"10px"}}/>622 Dixie Path,South Tobinchester 98336
+                </ContactItem>
+                <ContactItem>
+                    <PhoneIcon style={{marginRight:"10px"}} />+1 234 56 78
+                </ContactItem>
+                <ContactItem>
+                    <EmailOutlinedIcon style={{marginRight:"10px"}} />contact@lama.dev
+                </ContactItem>
+                <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer
